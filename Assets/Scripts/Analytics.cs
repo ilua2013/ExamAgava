8aa502d62fd31ef07daaf8e1ebb118b3f8e971b7
Илья <72304063+ilua2013@users.noreply.github.com>
2022-09-09 12:30:31 +0300

    Initial commit

---
+++ Assets/Scripts/Analytics.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Analytics : MonoBehaviour
+{
+    public void OnLevelComplete()
+    {
+        print("Всё выдержали нахуй, ахуенно, вот это парилка нахуй, эхх, за этой парилкой и надо жить");
+    }
+
+    public void OnChangeMoney()
+    {
+        print("Рубль будет хэ-хэ");
+    }
+}
